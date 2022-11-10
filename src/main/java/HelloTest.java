1c48ed83e6ae8e695c67c6fc997d0f431fc5a0f4
NailaNuriyev <nailanuriyev@gmail.com>
2022-11-10 15:49:38 -0800

    Added gitignore

--- src/main/java/HelloTest.java
+++ src/main/java/HelloTest.java
@@ -1,6 +1,8 @@
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello");
-        //adding to test fetch 
+        //adding to test fetch \
+        //work1c
+
     }
 }
